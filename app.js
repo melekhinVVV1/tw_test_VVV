f82f09d85a27b26dd36a609d9d531dabdd5b272a
v.melekhin <v.melekhin@timeweb.ru>
2023-12-12 16:47:49 +0300

    вернул порт 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = 8010;
+const port = 80;
 
 app.get('/', (req, res) => {
     res.send(`Hello, World! ${process.env.TITLE}`);
