955aaace6601747149aeaa161c347065e7f224e9
v.melekhin <v.melekhin@timeweb.ru>
2023-12-19 16:22:10 +0300

    main 1111

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const index = express();
+const main = express();
 require('dotenv').config();
 const port = 8000;
 
-index.get('/', (req, res) => {
+main.get('/', (req, res) => {
     res.send(`Hello, World! ${process.env.TITLE}`);
 });
 
 
-index.listen(port, () => {
+main.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
